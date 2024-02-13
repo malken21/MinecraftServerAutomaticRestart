6221a2f22c994b13c5f2ba62993bb62d5018c9fa
malken21 <77374813+malken21@users.noreply.github.com>
2024-02-13 21:47:50 +0900

    サーバー停止、確認スクリプトのスクリーンを特定できない場合がある問題修正

--- sudo-reboot.sh
+++ sudo-reboot.sh
@@ -13,9 +13,9 @@ sleep_time=5
 
 #-----設定部分----- end
 
-if [ -n "$(screen -ls $user_name/ | grep -o $screen_name)" ]; then
+if [ -n "$(screen -ls $user_name/ | grep -o $screen_name\s)" ]; then
     screen -p 0 -S $user_name/$screen_name -X eval 'stuff "stop"\015'
-    while [ -n "$(screen -ls $user_name/ | grep -o $screen_name)" ]; do
+    while [ -n "$(screen -ls $user_name/ | grep -o $screen_name\s)" ]; do
         sleep $sleep_time
     done
 fi
